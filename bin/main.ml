a1757d9cdd195c73348c43ae4f58f62729e7563b
Humam Alhusaini <humamalhusaini@tutamail.com>
2025-07-04 11:32:07 -0500

    got a simple example

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,9 @@
-let () = print_endline "Hello, World!"
+open Yojson
+open In_channel
+
+let () =
+  let chan = open_text "book.json" in
+  let buf = input_all chan in
+  let json1 = Basic.from_string buf in
+  let json2 = Basic.from_file "book.json" in
+  print_endline (if Yojson.Basic.equal json1 json2 then "OK" else "FAIL")
