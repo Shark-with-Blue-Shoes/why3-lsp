28b46e6a1065a59df6c7387db2d9bbf272b4cf96
Humam Alhusaini <humamalhusaini@tutamail.com>
2025-07-04 12:25:03 -0500

    got a good example

--- bin/main.ml
+++ bin/main.ml
@@ -1,9 +1,28 @@
 open Yojson
 open In_channel
+open Printf
 
 let () =
   let chan = open_text "book.json" in
   let buf = input_all chan in
-  let json1 = Basic.from_string buf in
-  let json2 = Basic.from_file "book.json" in
-  print_endline (if Yojson.Basic.equal json1 json2 then "OK" else "FAIL")
+  let json = Basic.from_string buf in
+  (* Locally open the JSON manipulation functions *)
+    let open Yojson.Basic.Util in
+  let title = json |> member "title" |> to_string in
+  let tags = json |> member "tags" |> to_list |> filter_string in
+  let pages = json |> member "pages" |> to_int in
+  let is_online = json |> member "is_online" |> to_bool_option in
+  let is_translated = json |> member "is_translated" |> to_bool_option in
+  let authors = json |> member "authors" |> to_list in
+  let names = List.map (fun json -> member "name" json |> to_string) authors  in
+(* Print the results of the parsing *)
+  printf "Title: %s (%d)\n" title pages;
+  printf "Authors: %s\n" (String.concat ", " names);
+  printf "Tags: %s\n" (String.concat ", " tags);
+  let string_of_bool_option =
+    function
+    | None -> "<unknown>"
+    | Some true -> "yes"
+    | Some false -> "no" in
+  printf "Online: %s\n" (string_of_bool_option is_online);
+  printf "Translated: %s\n" (string_of_bool_option is_translated)
