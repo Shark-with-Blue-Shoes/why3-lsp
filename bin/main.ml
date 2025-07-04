38b57e015d6346357fde297d6d56ceab3af6168b
Humam Alhusaini <humamalhusaini@tutamail.com>
2025-07-04 18:41:18 -0500

    got lwt working

--- bin/main.ml
+++ bin/main.ml
@@ -2,26 +2,8 @@ open Yojson
 open Yojson.Basic.Util
 open In_channel
 open Printf
+open Lwt
 
-module Id = struct
-  type t =
-    [ `String of string
-    | `Int of int
-    ]
-
-  let yojson_of_t = function
-    | `String s -> `String s
-    | `Int i -> `Int i
-  ;;
-
-  let t_of_yojson = function
-    | `String s -> `String s
-    | `Int i -> `Int i
-  ;;
-
-  let hash x = Hashtbl.hash x
-  let equal = ( = )
-end
 
 module Constant = struct
   let jsonrpc = "jsonrpc"
@@ -41,33 +23,27 @@ let assert_jsonrpc_version json =
     raise (Json_error ("invalid packet: jsonrpc version doesn't match " ^ jsonrpc))
 ;;
 
-let () =
-  let chan = open_text "book.json" in
-  let buf = input_all chan in
+let is_even num =  
+    match num mod 2 with 
+    | 0 ->  printf "number is even!"
+    | 1 -> printf "number is odd!"
+    | _ -> assert false
+
+let interp buf = 
   let json = Basic.from_string buf in
-  (* Locally open the JSON manipulation functions *)
   try
     assert_jsonrpc_version json;
-    let title = json |> member "title" |> to_string in
-    let tags = json |> member "tags" |> to_list |> filter_string in
-    let pages = json |> member "pages" |> to_int in
-    let is_online = json |> member "is_online" |> to_bool_option in
-    let is_translated = json |> member "is_translated" |> to_bool_option in
-    let authors = json |> member "authors" |> to_list in
-    let names = List.map (fun json -> member "name" json |> to_string) authors  in
-    (* Print the results of the parsing *)
-    printf "Title: %s (%d)\n" title pages;
-    printf "Authors: %s\n" (String.concat ", " names);
-    printf "Tags: %s\n" (String.concat ", " tags);
-    let string_of_bool_option =
-      function
-      | None -> "<unknown>"
-      | Some true -> "yes"
-      | Some false -> "no" in
-    printf "Online: %s\n" (string_of_bool_option is_online);
-    printf "Translated: %s\n" (string_of_bool_option is_translated)
+    let num = json |> member "num" |> to_int in 
+    is_even num;
   with
     | Yojson__Basic.Util.Type_error (x, p) -> printf "Type error: %s\n" x
     | Json_error err -> printf "Does not fulfill JSON RPC 2.0 protocol: %s" err
     | _ -> print_string "strange error"
-    
+
+let () =
+  let p : unit Lwt.t =
+    let%lwt line_1 = Lwt_io.(read_line stdin) in
+    let%lwt line_2 = Lwt_io.(read_line stdin) in
+    Lwt_io.printf "%s and %s\n" line_1 line_2
+  in
+  Lwt_main.run p
