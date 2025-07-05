af3a12831436f727b8272dcf94f624c63a8ce2bc
Humam Alhusaini <humamalhusaini@tutamail.com>
2025-07-05 14:10:58 -0500

    simplest version, will see if I can loop

--- test/testlsp.ml
+++ test/testlsp.ml
@@ -2,20 +2,13 @@ open Why3_lsp.Rpc
 
 (*Idea: have a read_line loop that appends data to a list, which fulfills a promise to start the processing*)
 
-let cmds = ref ["{ \"version\": \"2.0\", \"num\": 3 }"]
-
-let rec in_loop () = 
-  cmds := read_line () :: !cmds;
-  in_loop ();;
-
 let server () =
   let p = 
-    let%lwt input = (Lwt.return (List.hd !cmds)) in
+    let%lwt input = (Lwt_io.read_line Lwt_io.stdin) in
     Lwt.return (interp input) in
   Lwt_main.run p;;
 
-let () =  server (); in_loop ();
-
+let () =  server (); 
 (*
 {"version": "2.0", "num": 1}
 {"version": "2.0", "num": 5}
