0b36047e4bf70799957f25feae744f462bd4b7a5
Humam Alhusaini <humamalhusaini@tutamail.com>
2025-07-06 11:20:20 -0500

    basic queue trash

--- bin/main.ml
+++ bin/main.ml
@@ -1,7 +1,15 @@
-  open Why3_lsp.Rpc 
+open Why3_lsp.Rpc 
+open Queue
+open Thread
+
+let msg_queue = Queue.create ()
 
 let rec server () = 
   let input = read_line () in
-    interp input; server ();;
+    Queue.push input msg_queue;
+    server ();;
 
-let () = server ();
+let () = 
+  Queue.push "{\"version\": \"2.0\", \"num\": 5}" msg_queue; 
+  let _ = Thread.create (fun a -> Printf.printf "This is the Queue: %s\n" a) (Queue.pop msg_queue) in
+  server ();;
