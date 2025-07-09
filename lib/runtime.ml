7ef8133e87c78fce51d06888c6b3596434f7dc8d
Humam Alhusaini <humamalhusaini@tutamail.com>
2025-07-09 11:08:40 -0500

    added rgx for content type and content len

--- lib/runtime.ml
+++ lib/runtime.ml
@@ -1,3 +1,5 @@
+exception Rgx_failure of string
+
 open Yojson
 open Printf
 open Rpc
@@ -15,14 +17,14 @@ let has_id json =
   | _ -> true
 
 
-let get_headers str = 
+let get_header str = 
   try 
   let headers_rgx = "(.*)\\\\r\\\\n" in
     let rgx = Re.Perl.compile_pat headers_rgx in 
       let mtch = Re.exec rgx str in
         let headers = Re.Group.get mtch 1 in headers
   with
-    e -> printf "Error in getting headers: %s\n" (Printexc.to_string e); "I have nothing!";;
+    e -> raise (Rgx_failure (sprintf "Error in getting headers: %s\n" (Printexc.to_string e)));;
 
 let get_json str = 
   try 
@@ -31,28 +33,51 @@ let get_json str =
         let mtch = Re.exec rgx str in 
           let json = Re.Group.get mtch 0 in json
   with 
-    e -> printf "Error in getting json: %s\n" (Printexc.to_string e); "I have nothing!";; 
+    e -> raise (Rgx_failure (sprintf "Error in getting json: %s\n" (Printexc.to_string e)));;
 
-let sep_headers_and_json str = 
+let split_header_json str = 
     let json = get_json str in
-      let headers = get_headers str in
-              (headers, json);;
+      let headers = get_header str in
+              (headers, json)
+
+let get_content_len str = 
+  try
+    let rgx = Re.Perl.compile_pat "Content-Length: (\\d+)\\\\r\\\\n" in
+      let mtch = Re.exec rgx str in
+        let content_len = Re.Group.get mtch 1 in content_len
+  with 
+  | e -> raise (Rgx_failure (sprintf "Error in getting content length: %s\n" (Printexc.to_string e)));;
+
+let get_content_typ str = 
+  try
+    let rgx = Re.Perl.compile_pat "Content-Type: \"(.*)\"\\\\r\\\\n" in 
+      let mtch = Re.exec rgx str in 
+        let content_type = Re.Group.get mtch 1 in content_type
+  with 
+  | e -> raise (Rgx_failure (sprintf "Error in getting content length: %s\n" (Printexc.to_string e)));;
+
+let split_header str = 
+  let content_len = get_content_len str in 
+    let content_typ = get_content_typ str in 
+      (content_len, content_typ)
 
 let interp buf =
   try
-  let (header_str, json) = sep_headers_and_json buf in
-    printf "Headers: %s\n%!" header_str;
+  let (header_str, json) = split_header_json buf in
+    let (content_length, content_type) = split_header header_str in
+    printf "Content Length: %s, Content Type: %s\n%!" content_length content_type;
     let json = Basic.from_string json in
     if has_id json then
       let req = Request.t_of_yojson json in
-      printf "{id: %s, method: %s, params: %s}\n%!" (Id.to_str req.id) req.method_ (Basic.to_string (Structured.yojson_of_t req.params))
+      printf "{id: %s, method: %s, params: %s}\n\n%!" (Id.to_str req.id) req.method_ (Basic.to_string (Structured.yojson_of_t req.params))
     else
       let not = Notification.t_of_yojson json in 
-      printf "{method: %s, params: %s}\n%!" not.method_ (Basic.to_string (Structured.yojson_of_t not.params))
+      printf "{method: %s, params: %s}\n\n%!" not.method_ (Basic.to_string (Structured.yojson_of_t not.params))
   with
     | Missing_Member err -> printf "Missing Member: %s\n%!" err
     | Yojson__Basic.Util.Type_error (x, _) -> printf "Type error: %s\n%!" x
     | Json_error err -> printf "Does not fulfill JSON RPC 2.0 protocol: %s\n%!" err
+    | Rgx_failure err -> printf "Rgx_failure: %s\n%!" err
     | _ as e -> printf "Strange error: %s\n%!" (Printexc.to_string e)
 
 (* Consumer thread function: Continuously pops messages from the queue and processes them *)
@@ -99,7 +124,7 @@ let rec server_producer () =
       server_producer () (* Continue reading input *)
   | None ->
       (* End of input (Ctrl+D) reached. Signal consumer to shut down. *)
-      Printf.printf "[Producer] End of input (EOF) reached. Signaling consumer to shut down.\n%!";
+      Printf.printf "[Producer] End of input (EOF) reached. Signaling consumer to shut down.\n\n%!";
       Mutex.lock queue_mutex;
       shutdown_flag := true; (* Set shutdown flag *)
       Condition.signal queue_condition; (* Signal consumer one last time *)
