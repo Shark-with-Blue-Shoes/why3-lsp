3a4c85b727d9e9222ad80dc03e6acd80addf674c
Humam Alhusaini <humamalhusaini@tutamail.com>
2025-07-29 12:19:00 -0500

    figured out how to separate why3 init server and lsp server, by activating config later in the runtime

--- bin/main.ml
+++ bin/main.ml
@@ -1,19 +1,16 @@
 open Format
-open Why3
+open Printf
 
 open Why3_lsp.Server
 open Why3_lsp.Scheduler
 open Why3_lsp.Runtime
 
-module Main : functor () -> sig end
- = functor () -> struct
-
-(* files of the current task *)
-let files = Queue.create ()
+(* files of the current task 
+let files = Queue.create ()*)
 
 let quiet = ref false
 
-let spec =
+(*let spec =
   let open Getopt in
   [KLong "quiet", Hnd0 (fun () -> quiet := true),
    " remove all printing to stdout"]
@@ -24,14 +21,14 @@ let usage_str =
    Launch a command-line interface for Why3."
 
 let config, env =
-  Whyconf.Args.initialize spec (fun f -> Queue.add f files) usage_str
+  Whyconf.Args.initialize spec (fun f -> Queue.add f files) usage_str*)
 
 let () =
-  if Queue.is_empty files then
-    Whyconf.Args.exit_with_usage usage_str;
+  (*if Queue.is_empty files then
+    Whyconf.Args.exit_with_usage usage_str;*)
   let fmt = if !quiet then str_formatter else std_formatter in
-  fprintf fmt "Welcome to Why3 shell. Type 'help' for help.@.";
-  let dir =
+  printf "LSP Server (simplified) starting. Type messages and press Enter. Ctrl+D to exit.\n%!";
+  (*let dir =
     try
       Server_utils.get_session_dir ~allow_mkdir:true files
     with Invalid_argument s ->
@@ -44,7 +41,7 @@ let () =
       let f = Sysutil.concat (Sys.getcwd ()) f in
       send_request (Add_file_req f))
     files;
-  Server.init_server config env dir;
+  Server.init_server config env dir;*)
   Unix_scheduler.timeout ~ms:100
       (*a function that iterates over a list of notifications, applying treat_notification, returns true*) 
     (fun () -> List.iter
@@ -52,6 +49,3 @@ let () =
         true);
   Unix_scheduler.main_loop (interp fmt)
 
-end
-
-let () = Whyconf.register_command "shell" (module Main)
