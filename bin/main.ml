c26757cec12b334adade31a8d763560d313919d4
Humam Alhusaini <humamalhusaini@tutamail.com>
2025-07-31 11:15:52 -0500

    it seems that the protocol works by appending notifications to a list, treating those notifications, either by sending a request or it's own functionality. This seems quite strange for an LSP server, considering that JSON-RPC can either be a notif or request, not just a notif that may be translated to a request. The solution is to make my own LSP_Server with it's own coloring (inspired by why3's coloring), and whatnot.

--- bin/main.ml
+++ bin/main.ml
@@ -1,17 +1,16 @@
 open Format
 open Printf
 
-open Why3_lsp.Server
 open Why3_lsp.Scheduler
 open Why3_lsp.Runtime
 
 let () =
   let fmt = std_formatter in
   printf "LSP Server has begun!\n%!";
-  Unix_scheduler.timeout ~ms:100
+  (*Unix_scheduler.timeout ~ms:100
       (*a function that iterates over a list of notifications, applying treat_notification, returns true*) 
     (fun () -> List.iter
         (fun n -> treat_notification fmt n) (get_notified ()); 
-        true);
+        true);*)
   Unix_scheduler.main_loop (interp fmt);;
 
